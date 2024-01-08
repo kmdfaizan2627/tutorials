eba35265aa2d562789c8279f21c2d2f7adcd038f
DESKTOP-NQDI5OF\Dell <imran.khan9786@icloud.com>
2024-01-08 22:46:48 +0530

    changes

---
+++ Core-Java/src/com/condition/IFElse.java
@@ -0,0 +1,13 @@
+package com.condition;
+
+public class IFElse {
+	public static void main(String[] args) {
+		int a = 2;
+		if(a>3) {
+			System.out.println("A is greater than 3");
+		}else {
+			System.out.println("A is smaller than 3");
+		}
+	}
+
+}
---
+++ Core-Java/src/com/condition/If_If_Else.java
@@ -0,0 +1,16 @@
+package com.condition;
+
+public class If_If_Else {
+	public static void main(String[] args) {
+		int a=3;
+		if(a>3) {
+			System.out.println("A is greater than 3");
+		}if(a<3) {
+			System.out.println("A is smaller than 3");
+		}else {
+			System.out.println("A == 3");
+		}
+	
+	}
+
+}
